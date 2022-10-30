413b5c4410a22d69e65c6d850ba33fa8f1ac6ac5
lmh447669785 <447669785@qq.com>
2022-10-30 20:28:25 +0800

    更新 'src/data.py'

--- src/data.py
+++ src/data.py
@@ -55,13 +55,14 @@ def create_train_dataset(dataset,batch=16):
 
     device_num, rank_id = _get_rank_info()
 
-    #if device_num == 1 :
+    if device_num == 1 :
 
-        #train_ds = ds.GeneratorDataset(dataset, column_names=["input_images0","input_images1","input_images2","input_images3", "target_images"], shuffle=False,num_parallel_workers=8)
-    shard_id = get_rank() 
-    num_shards = get_group_size()
-    train_ds = ds.GeneratorDataset(dataset, column_names=["input_images0","input_images1","input_images2","input_images3", "target_images"], shuffle=False,num_parallel_workers=1,
-                                         num_shards=num_shards, shard_id=shard_id)
+        train_ds = ds.GeneratorDataset(dataset, column_names=["input_images0","input_images1","input_images2","input_images3", "target_images"], shuffle=False,num_parallel_workers=8)
+    else:
+        shard_id = get_rank() 
+        num_shards = get_group_size()
+        train_ds = ds.GeneratorDataset(dataset, column_names=["input_images0","input_images1","input_images2","input_images3", "target_images"], shuffle=False,num_parallel_workers=1,
+                                            num_shards=num_shards, shard_id=shard_id)
 
     train_ds = train_ds.batch(batch, drop_remainder=True)
 
