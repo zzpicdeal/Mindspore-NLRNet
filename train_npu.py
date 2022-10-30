fc2320c1ea4e4348bafdffc956dd9f017b32f792
lmh447669785 <447669785@qq.com>
2022-10-30 21:52:43 +0800

    更新 'train_npu.py'

--- train_npu.py
+++ train_npu.py
@@ -205,7 +205,7 @@ def main():
     lr = nn.piecewise_constant_lr(milestone,learning_rates)
 
     criterion = do_Loss()
-    optimizer = mindspore.nn.Adam(net.trainable_params(),learning_rate=0.0000625)
+    optimizer = mindspore.nn.Adam(net.trainable_params(),learning_rate=0.0005)
     trainwtihloss = CustomWithLossCell(net,criterion)
     model = Model(network=trainwtihloss, optimizer=optimizer)
     config_ck = CheckpointConfig(save_checkpoint_steps=data.train_dataset.get_dataset_size(),
